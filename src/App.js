ee8f32c41784a24da92a8fe2992873af8c9d6745
rhythmdev <rhythmbiswas@gmail.com>
2022-11-08 20:05:43 +0600

    basic setup checkup done

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import { Toaster } from 'react-hot-toast';
+import './App.css';
+
+function App() {
+  return (
+    <div >
+       <Toaster position="top-center"
+       reverseOrder={false}/>
+    </div>
+   
+  );
+}
+
+export default App;
