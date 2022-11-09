90b728ab881a9995d9b475c2f22d2c98132eb89c
rhythmdev <rhythmbiswas@gmail.com>
2022-11-09 20:20:21 +0600

    photo view package impiment done

--- src/Pages/Home/Services/ServiceCard.js
+++ src/Pages/Home/Services/ServiceCard.js
@@ -1,29 +1,34 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "react-photo-view/dist/react-photo-view.css";
+import { PhotoProvider, PhotoView } from "react-photo-view";
 
 const ServiceCard = ({ service }) => {
-  const {_id, title, image, price, description } = service;
+  const { _id, title, image, price, description } = service;
 
   return (
     <div className="bg-gray-100 p-6 rounded-md shadow-lg">
-      <img
-        className="object-cover w-full h-56 mb-6 rounded shadow-lg md:h-64 xl:h-80"
-        src={image}
-        alt=""
-      />
+      <PhotoProvider>
+        <PhotoView src={image}>
+          <img
+            className="object-cover w-full h-56 mb-6 rounded shadow-lg md:h-64 xl:h-80"
+            src={image}
+            alt=""
+          />
+        </PhotoView>
+      </PhotoProvider>
+
       <p className="mb-2 text-xl font-bold leading-none sm:text-2xl">{title}</p>
       <p className="text-gray-700 ">{description.slice(0, 100) + "....."}</p>
       <p className="text-gray-700 font-bold">Price: {price}$</p>
       <Link to={`/details/${_id}`}>
-      <button
-        type="button"
-        className="px-8 block w-full mt-4 py-3 font-semibold rounded-full bg-green-200 text-gray-800 hover:bg-green-400"
-      >
-        View Details
-      </button>
+        <button
+          type="button"
+          className="px-8 block w-full mt-4 py-3 font-semibold rounded-full bg-green-200 text-gray-800 hover:bg-green-400"
+        >
+          View Details
+        </button>
       </Link>
-
-      
     </div>
   );
 };
