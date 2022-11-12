b843ba4b151f6d884d87a94ea831e708f3de198c
rhythmdev <rhythmbiswas@gmail.com>
2022-11-12 15:47:16 +0600

    update review done

--- src/Pages/Reviews/EditReview/EditReview.js
+++ src/Pages/Reviews/EditReview/EditReview.js
@@ -29,19 +29,19 @@ const EditReview = () => {
 
   return (
     <div>
-      <h1 className="text-3xl font-bold text-center">Edit Review</h1>
-      <div className="flex justify-center">
+      <h1 className="text-3xl font-bold text-center mt-10">Edit Review</h1>
+      <div className="flex justify-center my-10">
         <div className="w-1/2">
           <form onSubmit={handelEditReview}>
             <div className="mb-4">
               <label
-                className="block text-gray-700 text-sm font-bold mb-2"
+                className="block text-gray-700 text-sm font-bold mb-4 text-center"
                 htmlFor="review"
               >
-                Review
+                Your Review
               </label>
               <textarea
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                className="shadow appearance-none border rounded w-full h-28 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline text-center font-semibold"
                 id="review"
                 type="text"
                 placeholder="Your Review..."
@@ -53,7 +53,7 @@ const EditReview = () => {
                 className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
                 type="submit"
               >
-                Update
+                Update Review
               </button>
             </div>
           </form>
